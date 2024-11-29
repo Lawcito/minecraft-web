d03207b7beea0e0b42f51232781539863a7c3def
NekoBamBam <coriannicolas21@gmail.com>
2024-11-29 02:15:51 -0300

    error

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Players from "./pages/Players";
 import Layout from "./layouts/Layout";
 import Memories from "./pages/Memories.Jsx";
 import Events from "./pages/Events.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/players" element={<Players />} />
           <Route path="/memories" element={<Memories />} />
           <Route path="/events" element={<Events />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";  
+import fondoerror from "../assets/404.jpeg"
+
+function NotFound() {
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <img src={fondoerror} alt="" className="w-full h-full object-cover" />
+    
+      
+      <div className="absolute inset-0 flex flex-col items-center justify-end text-center pb-10">
+        <h1 className="text-8xl font-bold text-blue-700">404</h1>
+        <h2 className="text-3xl md:text-5xl font-semibold mt-4 text-white">
+          Página no encontrada
+        </h2>
+        <Link to="/">  
+          <button className="mt-6 px-4 py-4 text-white bg-orange-900 rounded-lg hover:bg-black">
+            Regresar al inicio
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
